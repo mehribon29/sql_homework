785aaf6e1c76b646e9e33908a951249e22e5b398
mehribon29 <ymehribon@gmail.com>
2025-07-18 10:33:12 +0500

    Update hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -1,1 +1,21 @@
+1. data is the raw facts.
+  2. database is a collection of data.
+  3. relational database is a connected group of data in different tables. table is structured format used in database
+2.
+  1. can work with big GB, good storage, efficiency  
+  2. security 
+  3. fast 
+  4. recovery
+  5. integration 
+3.there are two ways: 
+  1. windows authentification 
+  2. separate username and password 
+  
 
+create table students (
+id INT,
+name VARCHAR(50),
+group_number INT PRIMARY KEY,
+age INT,
+grade CHAR NOT NULL
+)

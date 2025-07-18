79f42bd84c7f8b697013de27d7b096a11ea88049
mehribon29 <ymehribon@gmail.com>
2025-07-18 11:13:04 +0500

    Update hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -19,3 +19,16 @@ group_number INT PRIMARY KEY,
 age INT,
 grade CHAR NOT NULL
 )
+  
+Research and explain the different SQL commands: DQL, DML, DDL, DCL, TCL with examples.
+DQL-DATA QUERY LANGUAGE - it's a language to ask data 
+DML-DATA MALIPULATION LANGUAGE-it's a language to work with data with the following orders:select, insert, update, delete
+DDL-DATA DEFINITION LANGUAGE-it's a language to manage the stucture of the data with the foolowing orders: create, alter, drop, truncate 
+DCL-DATA CONTROL LANGUAGE-it's a language to control access to data stored in the database. It deals with permissions and security 
+TCL- transaction control language- is used to manage transactions in a database. transaction is a sequence of operations like insert, update,delete
+
+Write a query to insert three records into the Students table.
+select* from students
+insert into students (id, name,age) values (1, Ali, 20) 
+insert into students (id, name,age) values (2, Vali, 22) 
+insert into students (id, name,age) values (1, Mali, 19)
